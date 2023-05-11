fee066ade90fb0c59e283ccc72df51e789f36d07
YERMECHE Mohamed <myermeche2@outlook.fr>
2023-05-12 01:25:37 +0200

    step2: creation du model productImage avec le fichier signals.py pour la génération des vignettes

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-from catalog.models import Product
+from catalog.models import Product, ProductImage
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -10,3 +11,26 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Product, ProductAdmin)
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ("thumbnail_tag", "product_name")
+    readonly_fields = ("thumbnail",)
+    search_fields = ("product__name",)
+
+    """cette fonction renvoie du HTML pour la première colonne définie
+        dans la propriété list_display """
+
+    def thumbnail_tag(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="%s"/>' % obj.thumbnail.url)
+        return "-"
+
+    # ceci définit le nom de la colonne pour le list_display
+    thumbnail_tag.short_description = "Thumbnail"
+
+    def product_name(self, obj):
+        return obj.product.name
+
+
+admin.site.register(ProductImage, ProductImageAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -10,3 +10,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="product-images")
+    thumbnail = models.ImageField(upload_to="product-thumbnails", null=True)
