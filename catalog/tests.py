f713cf7648110955727509b575c5febaa61de989
YERMECHE Mohamed <myermeche2@outlook.fr>
2023-05-12 19:26:00 +0200

    step4: Test product_content et product_list

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+from .models import Product
+
+
+class ProductTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.product = Product.objects.create(
+            name="Bicyclette",
+            description="Un super vélô de competition",
+            category="Sport",
+            price=1250.99,
+        )
+
+    def test_product_content(self):
+        self.assertEqual(self.product.name, "Bicyclette")
+        self.assertEqual(self.product.description, "Un super vélô de competition")
+        self.assertEqual(self.product.category, "Sport")
+        self.assertEqual(self.product.price, 1250.99)
+
+    def test_product_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Un super vélô de competition")
+        self.assertTemplateUsed(response, "product_list.html")
