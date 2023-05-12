d165cedf0439381f64959a4fdcdd561ca1e53cf8
YERMECHE Mohamed <myermeche2@outlook.fr>
2023-05-12 20:11:11 +0200

    step6: Test de l'api

--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from catalog.models import Product
 
-# Create your tests here.
+
+class APITests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.product = Product.objects.create(
+            name="sneakers",
+            description="Une magnifique paire de baskets",
+            category="Sport",
+            price=250,
+        )
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("product_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertContains(response, self.product)
